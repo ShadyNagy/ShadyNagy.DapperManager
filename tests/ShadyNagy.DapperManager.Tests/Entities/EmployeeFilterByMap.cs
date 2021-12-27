9cb26e2026f03af7bab5d8f45819a39105a1ed5a
Shady Nagy <shadynagi@gmail.com>
2021-12-27 14:56:39 +0200

    fix

--- tests/ShadyNagy.DapperManager.Tests/Entities/EmployeeFilterByMap.cs
+++ tests/ShadyNagy.DapperManager.Tests/Entities/EmployeeFilterByMap.cs
@@ -4,15 +4,7 @@ namespace ShadyNagy.DapperManager.Tests.Entities
 {
   class EmployeeFilterByMap
   {
-    public DatabaseField SerialNumber { get; set; } = new DatabaseField(DatabaseFieldDirection.In, "SER_NO", DatabaseFieldType.Varchar2, null);
-    public DatabaseField ProjectNumber { get; set; } = new DatabaseField(DatabaseFieldDirection.In, "PROJ_NO", DatabaseFieldType.Varchar2, null);
-    public DatabaseField Status { get; set; } = new DatabaseField(DatabaseFieldDirection.In, "STATUS", DatabaseFieldType.Varchar2, null);
-    public DatabaseField TicketTypeCode { get; set; } = new DatabaseField(DatabaseFieldDirection.In, "TICKET_TYPE_CODE", DatabaseFieldType.Varchar2, null);
-    public DatabaseField TicketCategory { get; set; } = new DatabaseField(DatabaseFieldDirection.In, "TICKET_CATEGORY", DatabaseFieldType.Varchar2, null);
-    public DatabaseField TicketSubCategory { get; set; } = new DatabaseField(DatabaseFieldDirection.In, "TICKET_SUB_CATEGORY", DatabaseFieldType.Varchar2, null);
-    public DatabaseField Title { get; set; } = new DatabaseField(DatabaseFieldDirection.In, "DESCR", DatabaseFieldType.Varchar2, null);
-    public DatabaseField Result { get; set; } = new DatabaseField(DatabaseFieldDirection.Out, "P_RESULT", DatabaseFieldType.RefCursor, null);
-    public DatabaseField EnglishMessage { get; set; } = new DatabaseField(DatabaseFieldDirection.Out, "P_EN_MESS", DatabaseFieldType.Varchar2, null);
-    public DatabaseField ArabicMessage { get; set; } = new DatabaseField(DatabaseFieldDirection.Out, "P_AR_MESS", DatabaseFieldType.Varchar2, null);
+    public DatabaseField Id { get; set; } = new DatabaseField(DatabaseFieldDirection.In, "Id", DatabaseFieldType.Varchar2, 1);
+    public DatabaseField Name { get; set; } = new DatabaseField(DatabaseFieldDirection.In, "Name", DatabaseFieldType.Varchar2, null);
   }
 }
